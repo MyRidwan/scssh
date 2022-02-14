8406c71bc67882bf799a549177bc3a31cdb6f2d0
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-14 16:13:10 +0700

    Update vpn.sh

--- install/vpn.sh
+++ install/vpn.sh
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://github.com/myridwan/scssh/raw/main/modul/vpn.zip
+wget https://github.com/myridwan/scssh/raw/ipuk/modul/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/
