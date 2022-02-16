b3bea25e505a0308dc4b99480254a01c4bd3ee88
X£©GANSZ™ <92353970+MyRidwan@users.noreply.github.com>
2022-02-16 10:18:00 +0700

    Delete menu directory

--- menu/list-ssr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-a=$(cat /usr/local/shadowsocksr/akun.conf | sed 's/### //g')
-
-echo -e ">>_+==================+_<<"
-echo -e "||      User SSR        ||"
-echo -e ">>_+==================+_<<"
-echo -e "\n$a"
-echo -e "\nPremium Script Make by Ridwan"
--- menu/ubahhost.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-echo -e "Ubah host/domain untuk vps !"
-read -p "Domain: " domain
-echo "$domain" >> /etc/v2ray/domain
-sleep 1
-cert
